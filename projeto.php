09930065dcdc78ffbf719ee3e292d2403be0241d
pedrosilva22000 <pmss2002@gmail.com>
2024-04-07 20:22:23 +0100

    modified:   projeto.php

--- projeto.php
+++ projeto.php
@@ -1,79 +1,44 @@
 <?php
-require_once 'class.staff.php';
-require_once 'class.staff.php';
 require_once 'class.plugin.php';
-require_once 'class.api.projeto.php';
 require_once 'class.api.projeto.php';
 require_once 'util/table.installer.php';
 require_once 'config.php';
 
 include 'api.config.php';
 
-include INCLUDE_DIR . 'class.dispatcher.php';
-
-include 'debugger.php';
+include INCLUDE_DIR.'class.dispatcher.php';
 
 /* include INCLUDE_DIR.'class.signal.php'; */
 /* require 'api.inc.php'; */
 
 class ProjetoPlugin extends Plugin
 {
-class ProjetoPlugin extends Plugin
-{
 	var $config_class = 'ProjetoPluginConfig';
 
-	function bootstrap()
-	{
-		self::registerEndpoints();
+	function bootstrap(){
+        self::registerEndpoints();
 
-        $config = $this->getConfig();
-        $username = $config->get('username');
-
-		if ($this->firstRun()) {
-			$this->createDBTables();
-			$this->populateFirst($username);
+		if ($this->firstRun ()) {
+			
+			if (! $this->configureFirstRun ()) {
+				return false;
+			}
 		}
+		$this->getConfig();
 	}
 
-	function populateFirst($username)
-	{
-		try {
-			$staff = Staff::lookup($username);
-
-			$info = array(
-				'idStaff' => "{$staff->getId()}",
-				'isActive' => "1",
-				'canCreateTickets' => "1",
-				'canCloseTickets' => "1",
-				'canEditTickets' => "1",
-				'canSuspendTickets' => "1",
-				'notes' => "First Notes"
-
-			);
-
-			ApiProjeto::add($info, $erros);
-		} catch (Exception $e) {
-			echo 'An error occurred: ' . $e->getMessage();
-		}
+    function firstRun() {
+		$sql = 'SHOW TABLES LIKE \'' . TABLE_PREFIX.API_NEW_TABLE . '\'';
+		$res = db_query ( $sql );
+		return (db_num_rows ( $res ) == 0);
 	}
 
-	function firstRun()
-	{
-		$sql = 'SHOW TABLES LIKE \'' . TABLE_PREFIX . API_NEW_TABLE . '\'';
-		$res = db_query($sql);
-		return (db_num_rows($res) == 0);
-	}
-
-	function configureFirstRun()
-	{
-		if (!$this->createDBTables()) {
 	function configureFirstRun()
 	{
 		if (!$this->createDBTables()) {
 			echo "First run configuration error.  " . "Unable to create database tables!";
 			return false;
 		}
-
 
 
 		return true;
@@ -83,54 +48,47 @@ class ProjetoPlugin extends Plugin
 	{
 		$installer = new TableInstaller();
 		return $installer->install();
-	function createDBTables()
-	{
-		$installer = new TableInstaller();
-		return $installer->install();
 	}
 
 	private static function registerEndpoints()
 	{
 
-	private static function registerEndpoints()
-	{
+        $routes = array(
+            array(
+                'prefix' => "^/open/tickets",
+                'function' => 'create'
+            ),
+            array(
+                'prefix' => "^/close/tickets",
+                'function' => 'close'
+            ),
+            array(
+                'prefix' => "^/suspend/tickets",
+                'function' => 'suspend'
+            ),
+            array(
+                'prefix' => "^/requestApiKey/tickets",
+                'function' => 'requestApiKey'
+            ),
+            array(
+                'prefix' => "^/reopen/tickets",
+                'function' => 'reopen'
+            ),
+            array(
+                'prefix' => "^/edit/tickets",
+                'function' => 'edit'
+            )
+        );
 
-		$routes = array(
-			array(
-				'prefix' => "open/tickets",
-				'function' => 'create'
-			),
-			array(
-				'prefix' => "close/tickets",
-				'function' => 'close'
-			),
-			array(
-				'prefix' => "suspend/tickets",
-				'function' => 'suspend'
-			),
-			array(
-				'prefix' => "requestApiKey/tickets",
-				'function' => 'requestApiKey'
-			),
-			array(
-				'prefix' => "reopen/tickets",
-				'function' => 'reopen'
-			),
-			array(
-				'prefix' => "edit/tickets",
-				'function' => 'edit'
-			)
-		);
-
-		foreach ($routes as $route) {
-			Signal::connect('api', function ($dispatcher) use ($route) {
-				$dispatcher->append(
-					url_post(
-						"^/{$route['prefix']}\.(?P<format>xml|json|email)$",
-						array(INCLUDE_DIR . 'plugins/api/api.projeto.php:TicketApiControllerProjeto', $route['function'])
-					)
-				);
-			});
-		}
-	}
+        foreach ($routes as $route) {
+            Signal::connect('api', function ($dispatcher) use ($route) {
+                $dispatcher->append(
+                    url_post(
+                        "{$route['prefix']}\.(?P<format>xml|json|email)$",
+                        array(INCLUDE_DIR.'plugins/api/api.projeto.php:TicketApiControllerProjeto', $route['function'])
+                    )
+                );
+            });
+        }
+    }
 }
